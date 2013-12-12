aaaa86f4f5cb34e5894ae88d82020527c980c79c
Rosie Hoyem <rosiehoyem@gmail.com>
2013-12-12 12:08:55 -0500

    added seed data and layout nav menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+species = Species.create([{ common_name: 'London planetree', scientific_name: 'Platanus acerifolia' }, { common_name: 'Norway maple', scientific_name: 'Acer platanoides' }, 
+	{ common_name: 'Callery pear', scientific_name: 'Pyrus calleryana' }, { common_name: 'Honey locust', scientific_name: 'Gleditsia triacanthos' },
+	{ common_name: 'Pin oak', scientific_name: 'Quercus palustris' }, { common_name: 'Littleleaf linden', scientific_name: 'Tilia cordata' },
+	{ common_name: 'Green ash', scientific_name: 'Fraxinus pennsylvanica' }, { common_name: 'Red maple', scientific_name: 'Acer rubrum' },
+	{ common_name: 'Silver maple', scientific_name: 'Acer saccharinum' }, { common_name: 'Ginkgo', scientific_name: 'Ginkgo biloba' }])
